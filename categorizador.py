49c973a9597eebba55029d09bb69d08e95130de0
Laís Urano <lais.urano@gmail.com>
2024-08-01 13:07:54 -0300

    video 2.4

--- categorizador.py
+++ categorizador.py
@@ -6,48 +6,51 @@ dotenv.load_dotenv()
 client = anthropic.Anthropic(
     api_key=os.environ.get("ANTHROPIC_API_KEY"),
 )
-# modelo = "claude-3-5-sonnet-20240620"
-modelo = "claude-3-haiku-20240307"
-prompt_do_sistema = """
-Você é um categorizador de alimentos.
-Você deve assumir as categorias presentes na lista abaixo.
+modelo = "claude-3-5-sonnet-20240620"
+# modelo = "claude-3-haiku-20240307"
 
-# Lista de Categorias Válidas
-    - Frutas
-    - Vegetais
-    - Carnes
-    - Grãos e Cereais
-    - Laticínios
-    - Bebidas
-    - Doces e Sobremesas
-    - Alimentos Processados
+def categoriza_alimento(lista_de_categorias_validas,nome_do_alimento):
+    prompt_do_sistema = f"""
+    Você é um categorizador de alimentos.
+    Você deve assumir as categorias presentes na lista abaixo.
+    Você não deve responder informações não alimentícias e/ou que fogem do seu escopo principal.
 
-# Formato da Saída
-Produto: Nome do Produto
-Categoria: apresente a categoria do produto
+    # Lista de Categorias Válidas
+    {lista_de_categorias_validas.split(",")}
 
-# Exemplo de Saída
-Produto: Maçã
-Categoria: Frutas
-"""
-prompt_do_usuario = input("Forneça o nome do alimento: ")
+    # Formato da Saída
+    Produto: Nome do Produto
+    Categoria: apresente a categoria do produto
 
-message = client.messages.create(
-    model=modelo,
-    max_tokens=1000,
-    temperature=0,
-    system=prompt_do_sistema,
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": prompt_do_usuario
-                }
-            ]
-        }
-    ]
-)
-resposta = message.content[0].text
-print(resposta)
+    # Exemplo de Saída
+    Produto: Maçã
+    Categoria: Frutas
+    """
+    prompt_do_usuario = nome_do_alimento
+
+    message = client.messages.create(
+        model=modelo,
+        max_tokens=1000,
+        temperature=0,
+        system=prompt_do_sistema,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt_do_usuario
+                    }
+                ]
+            }
+        ]
+    )
+    resposta = message.content[0].text
+    return resposta
+
+categorias_validas = input("Informe as categorias válidas, separando por virgula: ")
+
+while True:
+    nome_do_alimento = input("Digite o nome do alimento: ")
+    texto_resposta = categoriza_alimento(categorias_validas,nome_do_alimento)
+    print(texto_resposta)
